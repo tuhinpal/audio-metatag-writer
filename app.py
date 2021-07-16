1972b23ca7342d27dbf3ba485b61b328a3dd7c38
cachecleanerjeet <51857187+cachecleanerjeet@users.noreply.github.com>
2021-07-16 13:55:59 +0530

    Trigger

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def send_js(path):
 #     app.debug = True
 #     app.run(host='0.0.0.0', port=port, use_reloader=True, threaded=True)
 
+# Production
 if __name__ == '__main__':
     app.debug = False
     app.run(host='0.0.0.0', port=port)
